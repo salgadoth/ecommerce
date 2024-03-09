4712a5db368114b7654a6794a1a39bff08dc32c7
Thiago <thiagobtw437@gmail.com>
2024-03-09 15:50:34 -0300

    Implemented microservice arch

---
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -0,0 +1,36 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.HashMap;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        var fraudService = new FraudDetectorService();
+        try(var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                new HashMap<>())){
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record){
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud.");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        System.out.println("------------------------------------------");
+
+        try {
+            Thread.sleep(5000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Order processed.");
+    }
+}
