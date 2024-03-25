801d99ef20d838f48d47f2760a6ab1688646e994
Thiago <thiagobtw437@gmail.com>
2024-03-25 20:21:05 -0300

    Added config for resetting the offset of a new consumer-groupe to the latest message

--- common-kafka/src/main/java/br/com/alura/ecommerce/consumer/ConsumerService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/consumer/ConsumerService.java
@@ -4,9 +4,10 @@ import br.com.alura.ecommerce.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public interface ConsumerService<T> {
-    void parse(ConsumerRecord<String, Message<T>> record) throws IOException;
+    void parse(ConsumerRecord<String, Message<T>> record) throws IOException, ExecutionException, InterruptedException;
     String getTopic();
     String getConsumerGroup();
 }
--- service-email-new-order/src/main/java/br/com/alura/ecommerce/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/alura/ecommerce/ecommerce/EmailNewOrderService.java
@@ -2,29 +2,22 @@ package br.com.alura.ecommerce.ecommerce;
 
 import br.com.alura.ecommerce.CorrelationId;
 import br.com.alura.ecommerce.Message;
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
 import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.math.BigDecimal;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var emailService = new EmailNewOrderService();
-        try (var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
+    public static void main(String[] args) {
+        new ServiceRunner<>(EmailNewOrderService::new).start(1);
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, preparing email");
         System.out.println(record.value());
@@ -37,5 +30,15 @@ public class EmailNewOrderService {
 
         emailDispatcher.send("ECOMMERCE_SEND_EMAIL", order.getEmail(), id, emailCode);
 
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 }
